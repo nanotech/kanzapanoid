2351e3219122ce9d3e283cf532f3513be82ca650
NanoTech <nanotech008@gmail.com>
2009-04-18 23:49:32 -0600

    Metal Boxes are now actual boxes.

--- items/metal_box/metal_box.rb
+++ items/metal_box/metal_box.rb
@@ -1,7 +1,17 @@
 class MetalBox < Item
 	def initialize(window, location)
-		body = CP::Body.new(0.1, 0.1)
-		shape = CP::Shape::Circle.new(body, 29, CP::Vec2.new(0.0, 0.0))
+		shape_array = [
+			CP::Vec2.new(-29, -29), # bottom left
+			CP::Vec2.new(-29, 29), # bottom right
+			CP::Vec2.new(29, 29), # top right
+			CP::Vec2.new(29, -29) # top left
+		]
+
+		mass = 29
+		inertia = CP.moment_for_poly(mass, shape_array, CP::Vec2.new(0,0))
+
+		body = CP::Body.new(mass, inertia)
+		shape = CP::Shape::Poly.new(body, shape_array, CP::Vec2.new(0,0))
 
 		super(window, location, shape)
 	end
